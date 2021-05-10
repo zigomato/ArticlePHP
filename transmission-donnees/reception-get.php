f13a63228f7f73613793af9be25774c85e06cbea
zigomato <zigomato.webmaster@gmail.com>
2021-05-10 14:22:47 +0200

    Create reception-get.php
    Mise en place du fichier php pour montrer la réception de données en utilisant la méthode GET

---
+++ transmission-donnees/reception-get.php
@@ -0,0 +1,9 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Réception par la méthode GET</title>
+	</head>
+	<body>
+		<p>Bienvenu <strong><?= $_GET['nom'] ?></strong>, tu as <strong><?= $_GET['age']?></strong>, ce n'est pas un peu vieux</p>
+	</body>
+</html>
