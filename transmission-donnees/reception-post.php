df821c0187e2c50e1368e808dfaafa191208db1c
zigomato <zigomato.webmaster@gmail.com>
2021-05-10 14:49:42 +0200

    Create reception-post.php
    Mise en place du fichier php pour illustrer l'envoi de données par la méthode POST

---
+++ transmission-donnees/reception-post.php
@@ -0,0 +1,9 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Réception par la méthode POST</title>
+	</head>
+	<body>
+		<p>Bienvenu <strong><?= $_POST['nom'] ?></strong>, tu as <strong><?= $_POST['age']?></strong>, tout compte fait, ce n'est pas un peu vieux</p>
+	</body>
+</html>
