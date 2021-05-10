dd7302c922a22560f5c983aef247b6b3ad5dbc64
zigomato <zigomato.webmaster@gmail.com>
2021-05-10 14:21:48 +0200

    Create envoi-get.php
    Création du fichier php pour illustrer la transmission de données en utilisant la méthode GET

---
+++ transmission-donnees/envoi-get.php
@@ -0,0 +1,9 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Envoi par la méthode GET</title>
+	</head>
+	<body>
+		<a href="/reception-age.php?nom=Nicolas&age=36">Envoi</a>
+	</body>
+</html>
