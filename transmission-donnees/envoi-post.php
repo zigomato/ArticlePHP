66cabc7fcdd992520f3e7969a3f30563dc824030
zigomato <zigomato.webmaster@gmail.com>
2021-05-10 14:49:19 +0200

    Create envoi-post.php
    Mise en place du fichier php pour illustrer l'envoi de données par la méthode POST

---
+++ transmission-donnees/envoi-post.php
@@ -0,0 +1,13 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Envoi par la méthode POST</title>
+	</head>
+	<body>
+		<form method="post" action="reception-post.php">
+			<input type="text" name="nom" value="Nicolas">
+			<input type="number" step=1 name="age" value=36>
+			<input type="submit" name="envoyer" value="Envoyer">
+		</form>
+	</body>
+</html>
